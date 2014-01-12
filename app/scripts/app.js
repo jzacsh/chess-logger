b9a55c9d9bb5e44f2889828424e98ed7fc81b024
Jonathan Zacsh <jzacsh@gmail.com>
2014-01-12 06:51:07 -0500

    no-op: reformatted app.js to preferred JS style.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,18 @@
 'use strict';
 
-angular.module('chessLoggerApp', [
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.
+    module('chessLoggerApp', [
+      'ngResource',
+      'ngSanitize',
+      'ngRoute'
+    ])
+    .config(function($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
